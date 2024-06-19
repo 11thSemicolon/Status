5651b391894a5477e336e8444578560f137d0428
11thSemicolon <ancientarceus7@gmail.com>
2024-06-19 18:04:31 -0400

    version 2

--- src/status/Status.java
+++ src/status/Status.java
@@ -7,13 +7,10 @@ package status;
 
 import java.util.Scanner;
 
-/** This class takes String input from user 
+/** This class takes enum input from user 
  * and calls method statusDetail to print the 
  * status of User-Rejected,Pending,Processing and Approved.
- * if the input is not given in String, 
- * program will accept and produce wrong results. 
- * change the code to use enums which avoids String input 
- * then print the status details. 
+ * Using enums avoids String input and produces reliable results.
  * @author srinivsi 
  */
 public class Status {
@@ -21,13 +18,15 @@ public class Status {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
-    {
-    Scanner in =new Scanner(System.in);
-    System.out.println("Enter the user status code (zero,one,two,three) in string");
-    String code = in.next();
-    StausUser t= new StausUser();
-    t.statusDetail(code); 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the user status code (ZERO, ONE, TWO, THREE):");
+        try {
+            StatusCode code = StatusCode.valueOf(in.next().toUpperCase());
+            StausUser statusUser = new StausUser();
+            statusUser.statusDetail(code);
+        } catch (IllegalArgumentException e) {
+            System.out.println("NOT VALID CODE");
+        }
     }
-    
 }
